8f9425b0865c269dff303b87df264c3444dbc80d
TaliD0506 <162917592+TaliD0506@users.noreply.github.com>
2025-10-01 23:47:33 +0200

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,8 @@
 -- 1. USERS
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(20),
