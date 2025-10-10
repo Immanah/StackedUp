8e8f7f8c62ee8a25b2b98afce5d96d7d06b2bc6e
Shafeeqah Mmadi <shafee.mmadi@gmail.com>
2025-10-10 03:17:33 +0200

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -420,7 +420,7 @@ function esc($s) {
                     </svg>
                 </a>
 
-                <a href="wishlist.html" class="icon-only" title="Wishlist" aria-label="Wishlist">
+                <a href="wishlist.php" class="icon-only" title="Wishlist" aria-label="Wishlist">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
                         <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" stroke="white" stroke-width="1.2" fill="none"/>
                     </svg>
@@ -815,4 +815,5 @@ function esc($s) {
         });
     </script>
 </body>
+
 </html>
